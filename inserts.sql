dc7cb8b63c3a30da360a29d2f7a4a4d12f73a788
Jordan <40555230+icedoesjs@users.noreply.github.com>
2022-11-23 17:40:25 -0500

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer (first_name, last_name, phone_num)
+VALUES ('Test', 'last', '555-674-1890');
+
+INSERT INTO ticket (ticket_price, seat_id)
+VALUES ('50', 456)
+
+INSERT INTO concessions (current_items, staff)
+VALUES ('[popcorn, water, candy]', '[joe, jim, jon]')
+
+INSERT INTO movie (movie_length, actors, show_dates)
+VALUES(98, '[actor1, actor2, actor3]', '[march15, april15, may15]')
